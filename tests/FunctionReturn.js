fad9bc87a3b9451ab6599c7877b27e30d287dfe5
Arturs Sefers <zanpakutos@gmail.com>
2014-11-27 14:33:32 +0200

    Added func return expr test.

---
+++ tests/FunctionReturn.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function test1() {
+	return "false";
+}
+
+function test2(x, y) {
+	return x + y;
+}
+
+function test3(value) {
+	return value;
+}
+
+func test_mantra(num:number, str:string):number {
+	return num;
+}
+
+test1();
+test2(10, 4.5);
+test3(false);
+test_mantra();
