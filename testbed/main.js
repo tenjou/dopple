5110d07102cf3c27c21fd6c0a2357fdd20a1943a
Arturs Sefers <zanpakutos@gmail.com>
2015-05-04 23:03:57 +0300

    Added testbed.

--- testbed/main.js
+++ testbed/main.js
@@ -67,8 +67,8 @@ function update()
 {
 	gl.clearColor(Math.random(), Math.random(), Math.random(), 1);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
-	window.requestAnimationFrame(update);
 }
 
 init();
 update();
+
